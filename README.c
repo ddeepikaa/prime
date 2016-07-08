ce3c3254d82668d486b79e2bbd99a17655d476d6
ddeepikaa <ddeepikaa@users.noreply.github.com>
2016-07-08 16:28:05 +0530

    finish

--- README.c
+++ README.c
@@ -3,18 +3,16 @@
 #include<stdio.h>
 int main()
 {
-  int num,ctr,size,count=1;
-  printf("enter the number");
-  scanf("%d",&num);
+  int num,num2,ctr,size,count=1;
+  printf("enter the range ");
+  scanf("%d",&num,&num2);
   size=num/2;
-  
 for(ctr=1;ctr<=size;ctr++)
 {
    if(num%ctr==0)
      count++;
-}
+    else 
+     continue;
 if(count==1)
-  printf("prime number");
-else 
-printf("not a prime number");
+  printf("%d",ctr);
 }
